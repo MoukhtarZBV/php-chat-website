9b69533eedd5bfc4c8952fedb321cfa078afb267
MoukhtarZBV <144065429+MoukhtarZBV@users.noreply.github.com>
2024-03-12 10:48:23 +0100

    Send message + get 10 last messages as divs

--- php/recuperer.php
+++ php/recuperer.php
@@ -1,5 +1,7 @@
 <?php
 
+require "dbConnection.php";
+
 function get10LastMessages(PDO $pdo) : array | bool {
     $stmt = $pdo->prepare("SELECT * FROM chatJS ORDER BY horaire DESC LIMIT 10");
 
@@ -10,8 +12,8 @@ function get10LastMessages(PDO $pdo) : array | bool {
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
-/*
-$messages = get10LastMessages();
+$pdo = createConnection();
+$messages = get10LastMessages($pdo);
 foreach ($messages as $message) {
-    echo 
-}*/
+    echo '<div class="message">'. $message["contenu"] .'</div>';
+}
